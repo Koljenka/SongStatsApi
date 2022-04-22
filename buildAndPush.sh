3ff75c5af715576c85c31386b41dc7cd2ac0a946
Kolja Nagel <kn685832@fh-muenster.de>
2022-04-22 15:06:48 +0200

    added clean to build

--- buildAndPush.sh
+++ buildAndPush.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 VERSION=0.1.1
+gradle clean
 gradle bootJar
 docker build -t kojenka/spotify-songstats-api:$VERSION .
 docker tag kojenka/spotify-songstats-api:$VERSION kojenka/spotify-songstats-api:latest
